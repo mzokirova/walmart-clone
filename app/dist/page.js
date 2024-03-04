a65af16a8f15f8784350298f8690e6cdb83027b0
Mashhura <mashhura2020@gmail.com>
2024-03-05 01:05:08 +0500

    UPDATE Grid Image Showcase DONE

---
+++ app/dist/page.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+function Home() {
+    return (React.createElement("main", { className: "flex-1" },
+        React.createElement("div", { className: "grid grid-cols-1 grid-flow-row-dense md:grid-cols-4 gap-5 m-5" },
+            React.createElement(GridOption, null))));
+}
+exports["default"] = Home;
