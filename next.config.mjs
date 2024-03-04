aaf87c4598fc196ba70c5a39bce8438575fc82e9
Mashhura <mashhura2020@gmail.com>
2024-03-05 00:34:20 +0500

    header done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                
+                protocol: 'https',
+                hostname: 'links.papareact.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'i.imgur.com',
+            },
+            
+        ]
+    }
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      colors: {
+        walmart:"#0071DC"
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
